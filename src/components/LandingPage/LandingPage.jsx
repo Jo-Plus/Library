61ff6fec04867dddb90d1e657a3abf2289b3a6b9
YousefSaeed <saeedyoussef219@gmail.com>
2025-10-19 23:50:01 +0300

    youssefUpdate

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar.jsx'
+import AutoSlider from '../AutoSlider/AutoSlider.jsx';
 
 export default function LandingPage() {
   return (<>
   <Navbar/>
+  <AutoSlider/>
   </>
   )
 }
